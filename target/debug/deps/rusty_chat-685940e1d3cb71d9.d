67710447968102f2829750539c9fa065728c127b
Riccardo Segala <72670063+RiccardoSegala04@users.noreply.github.com>
2023-04-01 14:13:51 +0200

    Delete target directory

--- target/debug/deps/rusty_chat-685940e1d3cb71d9.d
+++
@@ -1,6 +0,0 @@
-/Users/riccardosegala/Progetti/Rust/rusty-chat/target/debug/deps/rusty_chat-685940e1d3cb71d9: src/main.rs src/network.rs
-
-/Users/riccardosegala/Progetti/Rust/rusty-chat/target/debug/deps/rusty_chat-685940e1d3cb71d9.d: src/main.rs src/network.rs
-
-src/main.rs:
-src/network.rs:
