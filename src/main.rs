a925885384ef606ec419f54050562e93100f59a4
RiccardoSegala04 <riccardo.segala@icloud.com>
2023-04-02 21:57:14 +0200

    added basic command line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,50 @@ use crate::network::Peer;
 use std::io;
 use std::thread;
 use std::sync::{Arc, Mutex};
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about=None)]
+struct Args {
+    #[arg(short, long)]
+    name: String,
+
+    #[arg(short, long)]
+    address: String,
+
+    #[arg(short, long, action)]
+    server: bool,
+
+    #[arg(short, long, default_value_t=4444)]
+    port: u16,
+}
 
 fn main() {
-    //start_server();
-    start_client();
+    let args = Args::parse();
+
+    if args.server {
+        start_server(args.port, args.name.as_str());
+    } else {
+        start_client(args.address.as_str(), args.name.as_str());
+    }
 
     println!("Stopped!");
 }
 
-fn start_server() {
+fn start_server(port: u16, name: &str) {
     println!("Server started!");   
     
-    let mut peer = Peer::accept(4444, "Luca").unwrap();
+    let mut peer = Peer::accept(port, name).unwrap();
     
     println!("Connected to: {}", peer.get_name());
 
     chat_loop(&mut peer);
 }
 
-fn start_client() {
+fn start_client(address: &str, name: &str) {
     println!("Client started!");   
     
-    let mut peer = Peer::connect("localhost:4444", "Paolo").unwrap();
+    let mut peer = Peer::connect(address, name).unwrap();
     
     println!("Connected to: {}", peer.get_name());
 
@@ -62,12 +84,12 @@ fn chat_loop(peer: &mut Peer) {
         loop {
             let line = stream.recieve().unwrap();
 
-            print!("{}: {}", stream.get_name(), line);
-
             if line.eq("quit\n") || line.len()==0 {
                 stream.close();
                 break;
             }
+
+            print!("{}: {}", stream.get_name(), line);
 
         }
     });
