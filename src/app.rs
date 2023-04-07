ea7dc76cb4563934fde45f9415d20f1464b61869
RiccardoSegala04 <riccardo.segala@icloud.com>
2023-04-07 11:08:09 +0200

    Added message scrolling

--- src/app.rs
+++ src/app.rs
@@ -32,10 +32,19 @@ enum InputMode {
     Selecting,
 }
 
+struct Point {
+    line: u16,
+    col: u16,
+}
+
 pub struct App {
     input: String,
     input_mode: InputMode,
     messages: Arc<Mutex<Vec<String>>>,
+    
+    show_idx: usize,
+    cursor_pos: Point,
+
     closed: Arc<Mutex<bool>>,
     
     peer: Peer,
@@ -49,10 +58,12 @@ impl App {
             messages: Arc::new(Mutex::new(Vec::new())),
             closed: Arc::new(Mutex::new(false)),
             peer,
+            show_idx: 0,
+            cursor_pos: Point{line:0, col:0},
         }
     }
 
-    pub fn run(&mut self) -> io::Result<()> {
+   pub fn run(&mut self) -> io::Result<()> {
 
         enable_raw_mode().unwrap();
         let mut stdout = io::stdout();
@@ -60,7 +71,6 @@ impl App {
             stdout, 
             EnterAlternateScreen, 
             EnableMouseCapture, 
-            SetCursorShape(CursorShape::Line)
         ).unwrap();
         let backend = CrosstermBackend::new(stdout);
         let mut terminal = Terminal::new(backend).unwrap();
@@ -85,15 +95,18 @@ impl App {
                                 self.peer.close();
                                 *self.closed.lock().unwrap() = true;
                             }
-                            _ => {}
+                            _ => self.check_move_cursor(&key.code),
                         },
                         InputMode::Editing => match key.code {
                             KeyCode::Enter => {
-                                self.peer.send(self.input.as_str()).unwrap();
-                                self.messages.lock().unwrap().push(format!(
-                                    "You: {}", self.input
-                                ));
-                                self.input.clear();
+                                let to_send = self.input.trim();
+                                if !to_send.is_empty() {
+                                    self.peer.send(to_send).unwrap();
+                                    self.messages.lock().unwrap().push(format!(
+                                        "You: {}", to_send
+                                    ));
+                                    self.input.clear();
+                                }
                             }
                             KeyCode::Char(c) => {
                                 self.input.push(c);
@@ -110,7 +123,7 @@ impl App {
                             KeyCode::Esc => {
                                 self.input_mode = InputMode::Normal;
                             }
-                            _ => {}
+                            _ => self.check_move_cursor(&key.code),
                         }
                     }
                 }
@@ -194,41 +207,115 @@ impl App {
         f.render_widget(par, chunks[1]);
 
         let tmp = self.messages.lock().unwrap();
-        let mut messages: Vec<ListItem> = tmp
-            .iter()
-            .map(|m| {
-                let sep = m.find(' ').unwrap_or(0);
-                let content = vec![Spans::from(vec!(
-                        Span::styled(
-                            &m.as_str()[0..sep], 
-                            Style::default().add_modifier(Modifier::BOLD)),
-                            
-                        Span::raw(&m.as_str()[sep..m.len()])
-                    )
-                )];
-                ListItem::new(content)
-            })
-            .collect();
+
+        let chunk_size = chunks[0].height-1;
         
-        if(matches!(self.input_mode, InputMode::Editing)||self.input.len()>0) {
-            messages.push(ListItem::new(vec![Spans::from(vec!(
+        let mut stdout = io::stdout();
+        if matches!(self.input_mode, InputMode::Editing) {
+            if usize::from(chunk_size)<tmp.len() {
+                self.show_idx = tmp.len()-usize::from(chunk_size);
+            } 
+            execute!(stdout, SetCursorShape(CursorShape::Line)).unwrap();
+            self.cursor_pos.col = self.input.len() as u16 + 5;
+            self.cursor_pos.line = tmp.len() as u16;
+
+        } else {
+            execute!(stdout, SetCursorShape(CursorShape::Block)).unwrap();
+            if self.cursor_pos.line < (self.show_idx as u16) {
+                self.show_idx = usize::from(self.cursor_pos.line);
+            } else if self.cursor_pos.line+1>=(self.show_idx as u16)+chunks[0].height {
+                self.show_idx += 
+                    usize::from(
+                        self.cursor_pos.line+1-chunks[0].height)-self.show_idx;
+            }
+        }
+
+        let mut messages = Vec::new();
+
+        for i in 0..std::cmp::min(chunk_size, tmp.len().try_into().unwrap()) {
+            let i = i as usize;
+            let msg = tmp.get((i+self.show_idx) as usize).unwrap();
+            let sep = msg.find(' ').unwrap_or(0);
+            let content = vec![Spans::from(vec!(
                     Span::styled(
-                        "You: ", 
+                        &msg.as_str()[0..sep], 
                         Style::default().add_modifier(Modifier::BOLD)),
                         
-                    Span::raw(self.input.as_str())
-            ))]));
+                    Span::raw(&msg.as_str()[sep..msg.len()])
+                )
+            )];
+            messages.push(ListItem::new(content));
         }
+
+        messages.push(ListItem::new(vec![Spans::from(vec!(
+            Span::styled(
+                "You: ", 
+                Style::default().add_modifier(Modifier::BOLD)),
+                
+            Span::raw(self.input.as_str())
+        ))]));
 
         let messages = List::new(messages).block(Block::default());
         f.render_widget(messages, chunks[0]);
 
-        if matches!(self.input_mode, InputMode::Editing) {
-            f.set_cursor(
-                chunks[0].x + self.input.len() as u16 + 5,
-                chunks[0].y + tmp.len() as u16);
+        
+
+        let mut cursor_y = 0;
+        if self.cursor_pos.line>(self.show_idx as u16) {
+            cursor_y = self.cursor_pos.line-self.show_idx as u16;
         }
+        
+        f.set_cursor(
+            chunks[0].x + self.cursor_pos.col,
+            chunks[0].y + cursor_y
+        );
 
     }
+    
+    fn get_selected_line_len(&self) -> u16 {
+        self.messages.lock().unwrap().get(
+            usize::from(self.show_idx as u16 +self.cursor_pos.line)
+        ).unwrap_or(&"You: ".to_string()).len() as u16
+    }
+
+    fn check_move_cursor(&mut self, code: &KeyCode) {
+        match code {
+            KeyCode::Left => {
+                if self.cursor_pos.col>0 {
+                    self.cursor_pos.col-=1;
+                }
+            }
+            KeyCode::Right => {
+                if self.cursor_pos.col<self.get_selected_line_len()-1 {
+                    self.cursor_pos.col+=1;
+                }
+            }
+            KeyCode::Up => {
+                if self.cursor_pos.line>0 {
+                    self.cursor_pos.line-=1;
+
+                    self.cursor_pos.col = std::cmp::min(
+                        self.cursor_pos.col, 
+                        self.get_selected_line_len()-1
+                    ); 
+                } 
+            }
+            KeyCode::Down => {
+                if usize::from(self.cursor_pos.line)
+                    < self.messages.lock().unwrap().len() {
+
+                    self.cursor_pos.line+=1;
+                    
+                    self.cursor_pos.col = std::cmp::min(
+                        self.cursor_pos.col, 
+                        self.get_selected_line_len()-1
+                    ); 
+                }
+            }
+            _ => ()
+        }
+    }
+
+
 }
 
